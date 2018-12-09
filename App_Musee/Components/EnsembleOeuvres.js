87770a126bfd650052e14c9bcede6a2548ae90ec
dictusla <laurine.dictus@etu.utc.fr>
2018-12-09 21:47:06 +0100

    data comming from a request to the api

--- App_Musee/Components/EnsembleOeuvres.js
+++ App_Musee/Components/EnsembleOeuvres.js
@@ -1,25 +1,58 @@
 import React from 'react'
-import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native'
+import { StyleSheet, View, TextInput, Button, Text, FlatList, ActivityIndicator } from 'react-native'
 //import oeuvres from '../Helpers/BDMusee.js'
 //import AppBDMusee from '../Helpers/AppBDMusee.json'
 import ItemOeuvre from './ItemOeuvre'
 
 class EnsembleOeuvres extends React.Component {
 
-  _displayDetailForOeuvre = (idOeuvre, titleOeuvre, artisteOeuvre, image_pathOeuvre, materielOeuvre, themeOeuvre, dateOeuvre ) => {
-    //console.log("idOeuvre: " + idOeuvre);
-    this.props.navigation.navigate("DetailOeuvre", { idOeuvre: idOeuvre, titleOeuvre: titleOeuvre, artisteOeuvre: artisteOeuvre, image_pathOeuvre: image_pathOeuvre, materielOeuvre: materielOeuvre, themeOeuvre: themeOeuvre, dateOeuvre: dateOeuvre })
+  constructor(props){
+    super(props);
+    this.state ={ isLoading: true}
   }
 
-  render() {
-    const appBDMusee = require('../Helpers/AppBDMusee.json')
-    //console.log(appBDMusee.data)
+  componentDidMount(){
+    return fetch('http://192.168.1.45:8000/oeuvres/')
+      .then((response) => response.json())
+      .then((responseJson) => {
+	console.log(responseJson)
+
+        this.setState({
+          isLoading: false,
+          oeuvres: responseJson,
+        }, function(){
+
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+
+
+_displayDetailForOeuvre = (idOeuvre, titleOeuvre, artisteOeuvre, imageOeuvre, materielOeuvre, themeOeuvre, dateOeuvre ) => {
+    //console.log("idOeuvre: " + idOeuvre);
+    this.props.navigation.navigate("DetailOeuvre", { urlOeuvre: url, titleOeuvre: title, artisteOeuvre: artiste, imageOeuvre: photo, materielOeuvre: materiel, themeOeuvre: theme, dateOeuvre: date })
+  }
+
+  render(){
+
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+
+    
     return (
       <View style={styles.contents}>
         <View style={styles.main_container}>
           <FlatList
-            data={appBDMusee.data}
-            keyExtractor={(item) => item.id.toString()}
+            data={this.state.oeuvres}
+            keyExtractor={(item) => item.url}
             renderItem={({item}) => <ItemOeuvre oeuvre={item} displayDetailForOeuvre={this._displayDetailForOeuvre}/>}
           />
         </View>
